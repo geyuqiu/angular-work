9cf64e3c2d56191fd10d65b2ab5045b588257921
Yuqiu Ge <yuqiu.ge@gmail.com>
2024-10-17 20:17:29 +0200

    emit on enter

---
+++ src/app/components/hello-input/hello-input.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NameInputComponent } from '../name-input/name-input.component';
+
+@Component({
+  selector: 'app-hello-input',
+  standalone: true,
+  imports: [
+    NameInputComponent
+  ],
+  templateUrl: './hello-input.component.html',
+  styleUrl: './hello-input.component.scss'
+})
+export class HelloInputComponent {
+  nameFromInput: string;
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      this.nameFromInput = params.name;
+    });
+  }
+
+  handleNewName(newNameEvent: string) {
+    this.nameFromInput = newNameEvent;
+  }
+}

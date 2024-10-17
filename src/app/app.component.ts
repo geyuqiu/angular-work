79437fe499213a7c83d1729c5a0febcbd80ae916
Yuqiu Ge <yuqiu.ge@gmail.com>
2024-10-17 19:16:49 +0200

    add click to show text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HelloComponent } from './components/hello/hello.component';
 import { HelloListComponent } from './components/hello-list/hello-list.component';
+import { HelloClickComponent } from './components/hello-click/hello-click.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HelloComponent, HelloListComponent],
+  imports: [RouterOutlet, HelloComponent, HelloListComponent, HelloClickComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

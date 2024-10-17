8557ec18f87d2abb964544822e696fd07323c529
Yuqiu Ge <yuqiu.ge@gmail.com>
2024-10-17 03:39:57 +0200

    add input

--- src/app/components/hello/hello.component.ts
+++ src/app/components/hello/hello.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-hello',
@@ -8,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './hello.component.scss'
 })
 export class HelloComponent {
+  @Input()
   name = 'Sebastian';
 }

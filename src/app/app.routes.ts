c9c7543978ff9c842e069f46032af8e8fc23dd96
Yuqiu Ge <yuqiu.ge@gmail.com>
2024-10-17 23:40:04 +0200

    fix async

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,17 @@
 import { Routes } from '@angular/router';
 import { WeatherComponent } from './components/weather/weather.component';
 import { HelloComponent } from './components/hello/hello.component';
+import { HelloReactiveComponent } from './components/hello-reactive/hello-reactive.component';
 
 export const routes: Routes = [
   {path: '', component: HelloComponent},
   {
     path: 'weather',
     component: WeatherComponent,
+  },
+  {
+    path: 'reactive',
+    component: HelloReactiveComponent,
   },
   {path: '**', redirectTo: ''}
 ];

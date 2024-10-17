98cb3309379c7556a4891a734d3ccc81da8fa55a
Yuqiu Ge <yuqiu.ge@gmail.com>
2024-10-17 21:03:35 +0200

    hello-list for unit test

--- src/app/components/hello-list/hello-list.component.spec.ts
+++ src/app/components/hello-list/hello-list.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HelloListComponent } from './hello-list.component';
+import { By } from '@angular/platform-browser';
 
 describe('HelloListComponent', () => {
   let component: HelloListComponent;
@@ -19,5 +20,13 @@ describe('HelloListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('shows greetings for Paul, Robert and Matthias', () => {
+    const paragraphDebugElements = fixture.debugElement.queryAll(By.css('p'));
+    expect(paragraphDebugElements.length).toEqual(3);
+    expect((paragraphDebugElements[0].nativeElement as HTMLElement).innerText).toEqual('Hello Manuela');
+    expect((paragraphDebugElements[1].nativeElement as HTMLElement).innerText).toEqual('Hello Sebastian');
+    expect((paragraphDebugElements[2].nativeElement as HTMLElement).innerText).toEqual('Hello Abiram');
   });
 });

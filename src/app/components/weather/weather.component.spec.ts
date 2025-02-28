0424cacd210c07ac113a752a0a1cefe1dcbe38d0
Yuqiu Ge <yuqiu.ge@gmail.com>
2025-02-28 13:32:27 +0100

    add weather unit tests

--- src/app/components/weather/weather.component.spec.ts
+++ src/app/components/weather/weather.component.spec.ts
@@ -1,25 +1,50 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { of } from 'rxjs';
+import { WeatherService } from '../../services/weather.service';
+import { By } from '@angular/platform-browser';
 import { WeatherComponent } from './weather.component';
-import { provideHttpClient } from '@angular/common/http';
+import { CelsiusPipe } from '../../pipes/celsius.pipe';
+import { MockPipe } from 'ng-mocks';
 
 describe('WeatherComponent', () => {
   let component: WeatherComponent;
   let fixture: ComponentFixture<WeatherComponent>;
+  const expectedWeatherData
+    = {temp: 7, temp_min: 0, temp_max: 10};
+  let weatherServiceMock;
 
   beforeEach(async () => {
+    weatherServiceMock = jasmine.createSpyObj(['fetchWeather']);
+    weatherServiceMock.fetchWeather
+      .and.returnValue(of(expectedWeatherData));
     await TestBed.configureTestingModule({
-      imports: [WeatherComponent],
-      providers: [provideHttpClient()]
+      imports: [
+        WeatherComponent,
+        MockPipe(CelsiusPipe, (value) => value + '_transformedByMockPipe')
+      ],
+      providers: [
+        {provide: WeatherService, useValue: weatherServiceMock}
+      ]
     })
-    .compileComponents();
+      .compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(WeatherComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('shows temperatures', () => {
+    const temperatures = fixture.debugElement.queryAll(By.css('p'));
+    expect(temperatures.length).toEqual(3);
+    const actualTemperature = temperatures[0].nativeElement.innerText;
+    const minTemperature = temperatures[1].nativeElement.innerText;
+    const maxTemperature = temperatures[2].nativeElement.innerText;
+
+    expect(actualTemperature).toEqual('Aktuelle Temperatur: 7_transformedByMockPipe');
+    expect(minTemperature).toEqual('Min. Temperatur: 0_transformedByMockPipe');
+    expect(maxTemperature).toEqual('Max Temperatur: 10_transformedByMockPipe');
   });
 });
--- src/app/services/weather.service.spec.ts
+++ src/app/services/weather.service.spec.ts
@@ -1,16 +1,29 @@
 import { TestBed } from '@angular/core/testing';
 
 import { WeatherService } from './weather.service';
+import { of } from 'rxjs';
 
 describe('WeatherService', () => {
   let service: WeatherService;
+  let httpClientMock: any;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(WeatherService);
+    httpClientMock = jasmine.createSpyObj(['get']);
+    service = new WeatherService(httpClientMock);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('fetches WeatherData from API', (done) => {
+    const expectedWeatherData = {temp: 7, temp_min: 1, temp_max: 10};
+    const httpResponseMock = {body: {main: expectedWeatherData}};
+    httpClientMock.get.and.returnValue(of(httpResponseMock));
+    service.fetchWeather().subscribe(weatherData => {
+      expect(weatherData).toEqual(expectedWeatherData);
+      expect(httpClientMock.get).toHaveBeenCalledWith(
+        'http://api.openweathermap.org/data/2.5/weather?q=Munich,de&units=metric' as any,
+        {observe: 'response'} as any
+      );
+      done();
+    });
   });
 });
+

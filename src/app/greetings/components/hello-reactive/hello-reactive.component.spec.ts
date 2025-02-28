d68639a5571eab87d5d354947814be7713aad8bd
Yuqiu Ge <yuqiu.ge@gmail.com>
2025-02-28 15:24:24 +0100

    mock ActivatedRoute

--- src/app/greetings/components/hello-reactive/hello-reactive.component.spec.ts
+++ src/app/greetings/components/hello-reactive/hello-reactive.component.spec.ts
@@ -2,14 +2,23 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HelloReactiveComponent } from './hello-reactive.component';
 import { By } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
 
 describe('HelloReactiveComponent', () => {
   let component: HelloReactiveComponent;
   let fixture: ComponentFixture<HelloReactiveComponent>;
 
   beforeEach(async () => {
+    let mockActivatedRoute = {
+      params:  new Subject().asObservable(), // Use the Subject as an observable
+    };
     await TestBed.configureTestingModule({
-      imports: [HelloReactiveComponent]
+      imports: [HelloReactiveComponent, ReactiveFormsModule],
+      providers: [
+        { provide: ActivatedRoute, useValue: mockActivatedRoute } // Provide mock
+      ]
     })
       .compileComponents();
   });
--- src/app/greetings/components/hello/hello.component.spec.ts
+++ src/app/greetings/components/hello/hello.component.spec.ts
@@ -2,6 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HelloComponent } from './hello.component';
 import { By } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 
 describe('HelloComponent', () => {
   let component: HelloComponent;
